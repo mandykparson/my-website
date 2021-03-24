2bfcd6a5b0cb3ea06fafd04803b4cb1002c35507
Mandy <mandykparson@gmail.com>
2021-03-24 15:16:32 -0600

    added consts and prizes

---
+++ index.js
@@ -0,0 +1,10 @@
+const takeANumber = "" // the number generated when a use clicks on the Take a Number button
+const eneteredNumber = "" // the number inputed (may not be takeANumber) by user
+const prizes = [
+    "a puppy", 
+    "a company mug", 
+    "a discounted christmas sweater from Walmart", 
+    "the CD: Now That's What I Call Music 14", 
+    "Kazaam on BlueRay", 
+]
+
