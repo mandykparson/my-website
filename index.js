f3b8ce95e4d3f10689734f0fe6668913d183a35d
Mandy <mandykparson@gmail.com>
2021-04-24 10:39:15 -0600

    turning headers into buttons

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ const newInput = document.getElementById('myProjects');
 newInput.addEventListener('click', function() {
   alert('More to come!');
 }); 
+
+function showhide1() {
+  const div = document.getElementById("aboutMe");
+  div.classList.toggle('hidden'); 
+}
+
+function showhide2() {
+  const div = document.getElementById("myProjects");
+  div.classList.toggle('hidden'); 
+}
+
+function showhide3() {
+  const div = document.getElementById("emailMe");
+  div.classList.toggle('hidden'); 
+}
